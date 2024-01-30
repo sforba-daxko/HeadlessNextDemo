9480b912240bacb17f33723652e8f8ed2044253b
Scott Forba <scott.forba@chi-m-2331.myfiosgateway.com>
2024-01-30 14:52:23 -0500

    nav render and fix svg issues

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Header from "./layout/header";
 import Hero from "./components/hero";
+import Feature from "./components/feature";
 export default function Home() {
   return (
     <>
       <Header />
       <main >
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <Hero/>
+        <Feature />
       </main>
     </>
 
