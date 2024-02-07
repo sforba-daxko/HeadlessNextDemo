70fe0aab49806e01b6b18a6cc053105c339dafc5
Scott Forba <scott.forba@chi-m-2331.myfiosgateway.com>
2024-02-07 16:14:15 -0500

    setup component render

--- app/components/feature.js
+++ app/components/feature.js
@@ -1,10 +1,19 @@
-const Feature = () => {
+const Feature = (props) => {
+
+    const {copy, headline} = props || {};
     return(
         <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <div className="max-w-screen-md mb-8 lg:mb-16">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-                <p className="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                {headline && 
+                    (<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                        {headline}
+                    </h2>)
+                }
+                {copy && (
+                    <div className="text-gray-500 sm:text-xl dark:text-gray-400" dangerouslySetInnerHTML={{ __html: copy }}/>
+                )}
+
             </div>
             <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
                 <div>
--- app/page.js
+++ app/page.js
@@ -2,15 +2,91 @@ import Image from "next/image";
 import Header from "./layout/header";
 import Hero from "./components/hero";
 import Feature from "./components/feature";
-export default function Home() {
+
+
+const getHomePageData = async () => {
+  const res = await fetch(process.env.WP_GRAPHQL_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: `query HomePageQuery {
+        page(id: "cG9zdDo2") {
+          content
+          pageFields {
+            fields {
+              fieldGroupName
+              ... on PageFieldsFieldsHeroLayout {
+                __typename
+                hero {
+                  copy
+                  __typename
+                  title
+                  cta {
+                    target
+                    title
+                    url
+                  }
+                }
+              }
+              ... on PageFieldsFieldsFeatureLayout {
+                fieldGroupName
+                feature {
+                  copy
+                  fieldGroupName
+                  headline
+                }
+              }
+            }
+          }
+        }
+      }`
+    })
+  })
+
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+  const responseJson = await res?.json();
+  return responseJson?.data?.page?.pageFields;
+}
+
+
+const Home = async () => {
+  const homeData = await getHomePageData();
+  const {fields} = homeData;
+
+  const renderComponents = () => {
+    return fields && fields.map((field, index)=> {
+      let component = null;
+      const {fieldGroupName} = field || {};
+      switch (fieldGroupName) {
+        case 'PageFieldsFieldsHeroLayout':
+          component = <Hero {...field?.hero} key={`${fieldGroupName}_${index}`}/>;
+          break;
+        case 'PageFieldsFieldsFeatureLayout':
+          component = < Feature {...field?.feature} key={`${fieldGroupName}_${index}`}/>;
+          break;          
+        default:
+          component =<div key={`null_${index}`}/>;
+          break;
+      }
+      return component;
+
+    })
+  }
   return (
     <>
       <Header />
       <main >
-        <Hero/>
-        <Feature />
+        {renderComponents()}
       </main>
     </>
 
   );
 }
+
+export default Home;
